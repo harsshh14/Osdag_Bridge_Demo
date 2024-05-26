b96f697ffc5a235f18cab5b66bf1894490740108
Harsshh Wankhayday <harsshhwankhayday123@gmail.com>
2024-05-26 17:17:07 +0530

    Added the function to automate K matrix

--- ILD_Transverse.py
+++ ILD_Transverse.py
@@ -7,16 +7,56 @@ def ILD_Transverse_two_girders(width,space_vectorization,overhang_len,girder_spa
 #function to find the support reactions
 #width= width of bridge, space_vectorization = space of load during vectorization
     #num_points=(width/space_vectorization)+1
-    rxn_A=[None]*(math.ceil(width/space_vectorization)+1)
-    rxn_B=[None]*(math.ceil(width/space_vectorization)+1)
+    rxn_A=np.empty(math.ceil(width/space_vectorization)+1)
+    rxn_B=np.empty(math.ceil(width/space_vectorization)+1)
 
     for x in range(len(rxn_A)):
         rxn_B[x]=(x*space_vectorization-overhang_len)/girder_space
         rxn_A[x]=1-rxn_B[x]    
-    
 
+# ILD_Transverse_two_girders(5,0.25,1.25,2.5)
 
-#ILD_Transverse_two_girders(5,0.25,1.25,2.5)
+def create_k_matrix(E,I,girder_space):
+    k_mat=np.zeros((4, 4))
+    k_mat[0][0]=k_mat[2][2]=12/girder_space**3
+    k_mat[0][1]=k_mat[1][0]=k_mat[0][3]=k_mat[3][0]=6/girder_space**2
+    k_mat[0][2]=k_mat[2][0]=-12/girder_space**3
+    k_mat[1][1]=k_mat[3][3]=4/girder_space
+    k_mat[1][2]=k_mat[2][1]=k_mat[2][3]=k_mat[3][2]=-6/girder_space**2
+    k_mat[1][3]=k_mat[3][1]=2/girder_space
+    return k_mat
+
+def K_matrix(num_supp, I,E, girder_space):
+    #supp is the no of supp(girders). for a three girder bridge, num_supp=3
+    K= np.zeros((num_supp*2,num_supp*2 ))
+    k_1=create_k_matrix(E,I,girder_space)
+    temp=num_supp
+    l=1
+    while temp>1:
+        i=0
+        for x in [num_supp+l,l,num_supp+l+1,l+1]:
+            j=0
+            for y in [num_supp+l,l,num_supp+l+1,l+1]:
+                K[x-1][y-1]+=k_1[i][j]
+                j+=1
+            i+=1
+        l+=1
+        temp =temp- 1
+    return K
+
+def ILD_Transverse_three_girder(width,space_vectorization,overhang_len,girder_space, height_slab,fck,num_supp):
+    #height of slab is needed to calculate I(Moment of inertia)
+    #Grade of concrete is needed to calculate E
+    #num_supp is the number of supports(girders)
+    b=1000 #mm (length of 1 m section of road is considered )
+    E=5000*math.sqrt(fck)  #MPa
+    I=b*height_slab**3/12 #mm4
+
+    #a code for K matrix has been created in the trial.py file. check it
+    K= K_matrix(num_supp,I,E,girder_space)
+    print(K)
+
+ILD_Transverse_three_girder(0,0,0,2.5,0,0,3)
 ################################################################
 
 
@@ -33,4 +73,4 @@ def vectorize_load(width, space_vectorization,load,x_min, x_max):
             temp_load_vec[x]=load*space_vectorization
     load_vec.append(temp_load_vec)
     print(load_vec)
-vectorize_load(10,0.25,16,9.5,10)
+# vectorize_load(10,0.25,16,9.5,10)
--- trial.py
+++ trial.py
@@ -1,15 +1,50 @@
-# Step 1: Initialize an empty list
-matrix = []
+# # Step 1: Initialize an empty list
+# matrix = []
 
-# Step 2: Define rows (lists) to append to the matrix
-row1 = [1, 2, 3]
-row2 = [4, 5, 6]
-row3 = [7, 8, 9]
+# # Step 2: Define rows (lists) to append to the matrix
+# row1 = [1, 2, 3]
+# row2 = [4, 5, 6]
+# row3 = [7, 8, 9]
 
-# Step 3: Append rows to the matrix
-matrix.append(row1)
-matrix.append(row2)
-matrix.append(row3)
+# # Step 3: Append rows to the matrix
+# matrix.append(row1)
+# matrix.append(row2)
+# matrix.append(row3)
 
-# Printing the resulting matrix
-print(matrix[1][1])
+# # Printing the resulting matrix
+# print(matrix[1][1])
+
+
+import numpy as np
+
+def create_k_matrix(E,I,girder_space):
+    k_mat=np.zeros((4, 4))
+    k_mat[0][0]=k_mat[2][2]=12/girder_space**3
+    k_mat[0][1]=k_mat[1][0]=k_mat[0][3]=k_mat[3][0]=6/girder_space**2
+    k_mat[0][2]=k_mat[2][0]=-12/girder_space**3
+    k_mat[1][1]=k_mat[3][3]=4/girder_space
+    k_mat[1][2]=k_mat[2][1]=k_mat[2][3]=k_mat[3][2]=-6/girder_space**2
+    k_mat[1][3]=k_mat[3][1]=2/girder_space
+    return k_mat
+
+def K_matrix(num_supp, I,E, girder_space):
+    #supp is the no of supp(girders). for a three girder bridge, num_supp=3
+    K= np.zeros((num_supp*2,num_supp*2 ))
+    k_1=create_k_matrix(E,I,girder_space)
+    temp=num_supp
+    l=1
+    while temp>1:
+        i=0
+        for x in [num_supp+l,l,num_supp+l+1,l+1]:
+            j=0
+            for y in [num_supp+l,l,num_supp+l+1,l+1]:
+                K[x-1][y-1]+=k_1[i][j]
+                j+=1
+            i+=1
+        l+=1
+        temp =temp- 1
+
+
+    print(K)        
+
+K_matrix(3,0,0,2.5)
